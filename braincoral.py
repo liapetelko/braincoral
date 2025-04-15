cd8f5ccaba448df37e8939191999c097ae51bb51
liapetelko <98822243+liapetelko@users.noreply.github.com>
2025-04-15 11:41:59 +0300

    Update braincoral.py

--- braincoral.py
+++ braincoral.py
@@ -27,12 +27,29 @@ import os
     
 #     st.session_state.firebase_initialized = True
 
-cred = credentials.Certificate(r"brain-coral-vocab-firebase-adminsdk-c6g3m-b019f7d8ba.json")
+#cred = credentials.Certificate(r"brain-coral-vocab-firebase-adminsdk-c6g3m-b019f7d8ba.json")
+
+#if not firebase_admin._apps:
+    #firebase_admin.initialize_app(cred)
+#brain = firestore.client()
 
 if not firebase_admin._apps:
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["firebase"].get("universe_domain", "googleapis.com")
+    })
     firebase_admin.initialize_app(cred)
-brain = firestore.client()
 
+brain = firestore.client()
 
 st.markdown(
     """
